6839db4934bfb05a506919b3800ba3ee20a80727
EloraBarua <elorabarua62@gmail.com>
2023-07-25 23:29:28 +0600

    project initialized

---
+++ app.js
@@ -0,0 +1,21 @@
+// import packages
+const express = require('express');
+const cors = require('cors');
+
+
+// create app
+const app = express();
+
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+
+// Routes
+// base route
+app.get('/' , (req,res) => {
+    res.send('app is running');
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,11 @@
+// import app
+const app = require("./app");
+
+
+// server running port
+const port = process.env.PORT || 5000;
+
+
+app.listen(port , () => {
+    console.log(`App is running on port ${port}`);
+})
